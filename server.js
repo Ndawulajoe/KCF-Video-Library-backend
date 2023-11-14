f5e69d533d51ed238bdb702bc8624478222d8bb9
NdawulaJoe <ndawulajoseph255@gmail.com>
2023-11-14 21:09:17 +0300

    second commite

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const http = require('http');
 // import app from './app';
-const app = require('./src/app')
+const app = require('./app')
 const PORT = process.env.PORT || 3002;
 const server = http.createServer(app);
+// server.listen(PORT)
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
